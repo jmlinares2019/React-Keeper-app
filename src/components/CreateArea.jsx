2a78b2dd15f84c75dedcaf2ba2aa5ba510ebcfe6
jmlinares2019 <jlinaresmar@gmail.com>
2024-01-30 21:13:34 +0100

    Implement the add note functionality. - Keeping track of the title and content in the Create Area component. - Pass the new note back to the App. - Add new note to an array and render separate Note components for each item.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+    // Create a constant that keeps track of the title and content.
+    const [newNote, setNewNote] = useState({
+        title: "",
+        content: "",
+    });
+    function handleChange(e){
+        const { name, value } = e.target;
+        setNewNote(function(prevValues){        
+            if(name === "title"){
+                return {
+                    title: value,
+                    content: prevValues.content
+                } 
+            } else if(name === "content"){
+                return {
+                    title: prevValues.title,
+                    content: value
+                }
+            }
+        });
+    }
+
     return (
         <div>
             <form>
-                <input name="title" placeholder="Title" />
-                <textarea name="content" placeholder="Take a note..." rows="3" />
-                <button>Add</button>
+                <input 
+                    name="title" 
+                    placeholder="Title" 
+                    onChange={handleChange} 
+                    value={newNote.title}/>
+                <textarea 
+                    name="content" 
+                    placeholder="Take a note..." 
+                    rows="3" 
+                    onChange={handleChange} 
+                    value={newNote.content}/>
+                {/* Pass the new note back to the App. */}
+                <button onClick={function(e){
+                    e.preventDefault();
+                    props.onAdd(newNote);
+                    setNewNote({
+                        title: "",
+                        content: ""
+                    });
+                }}>
+                Add
+                </button>
             </form>
         </div>
     );
